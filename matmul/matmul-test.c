08da5b348d0169aa48391016c36918295936fbf6
b4rtaz <b4rtaz@gmail.com>
2023-12-03 17:10:37 +0100

    matmul slice.

--- matmul/matmul-test.c
+++ matmul/matmul-test.c
@@ -2,8 +2,64 @@
 #include <stdlib.h>
 #include <string.h>
 #include "matmul.h"
+#include "matmul-slice.h"
 
-void read_floats(float* target, int n, char* path) {
+int n = 4096;
+int d = 4096;
+float* input; // (4096,1)
+float* weights; // (4096x4096)
+float* output; // (4096,1)
+float* expectedOutput;
+
+void test0_default() {
+    matmul(output, input, weights, n, d);
+}
+
+void test0_distr() {
+    int slices = 8;
+
+    struct matmul_slice slice;
+    matmul_slice_new(&slice, slices, n, d);
+
+    for (int s = 0; s < slices; s++) {
+        float* weights0 = calloc(slice.weights0Length, sizeof(float));
+
+        int weightsOffset = matmul_slice_split_weights(&slice, s, weights, weights0);
+
+        float* output0 = calloc(slice.d0, sizeof(float));
+        matmul(output0, input, weights0, n, slice.d0);
+
+        int outputOffset = matmul_slice_merge_output(&slice, s, output, output0);
+
+        free(weights0);
+        free(output0);
+
+        printf("weights <%8d, %8d> output <%4d, %4d>\n",
+            weightsOffset,
+            weightsOffset + slice.weights0Length,
+            outputOffset,
+            outputOffset + slice.d0);
+    }
+}
+
+void compareOrFail(char *name) {
+    int ix = -1;
+    for (int i = 0; i < d; i++) {
+        if (output[i] != expectedOutput[i]) {
+            ix = i;
+            break;
+        }
+    }
+    if (ix < 0) {
+        printf("[%s] ✅\n", name);
+    } else {
+        printf("[%s] ❌ ix=%d\n", name, ix);
+        printf("%f != %f\n", output[ix], expectedOutput[ix]);
+        exit(-1);
+    }
+}
+
+void readData(float* target, int n, char* path) {
     FILE* f = fopen(path, "r");
     if (f == NULL) exit(-1);
     fread(target, sizeof(float), n, f);
@@ -12,32 +68,25 @@ void read_floats(float* target, int n, char* path) {
 }
 
 int main() {
-    int n = 4096;
-    int d = 4096;
+    input = calloc(n, sizeof(float));
+    weights = calloc(n * d, sizeof(float));
+    output = calloc(d, sizeof(float));
+    expectedOutput = calloc(d, sizeof(float));
+    readData(input, n, "matmul/test0-input.data");
+    readData(weights, n * d, "matmul/test0-weights.data");
+    readData(expectedOutput, d, "matmul/test0-output.data");
 
-    float* input = calloc(n, sizeof(float));
-    float* weights = calloc(n * d, sizeof(float));
-    float* output = calloc(d, sizeof(float));
-    float* expectedOutput = calloc(d, sizeof(float));
-    read_floats(input, n, "matmul/test0-input.data");
-    read_floats(weights, n * d, "matmul/test0-weights.data");
     memset(output, 0, d * sizeof(float));
-    read_floats(expectedOutput, d, "matmul/test0-output.data");
+    test0_default();
+    compareOrFail("default");
 
-    matmul(output, input, weights, n, d);
+    memset(output, 0, d * sizeof(float));
+    test0_distr();
+    compareOrFail("distr");
 
-    int r = memcmp(output, expectedOutput, n * sizeof(float));
     free(input);
     free(weights);
     free(output);
     free(expectedOutput);
-
-    printf("r = %d\n", r);
-    if (r == 0) {
-        printf("✅ Test passed!\n");
-        return 0;
-    } else {
-        printf("❌ Test failed!\n");
-        return -1;
-    }
+    return 0;
 }
--- matmul/matmul.h
+++ matmul/matmul.h
@@ -1,4 +1,14 @@
+
+//     weights      input    output
+//   ___________     ___      ___
+//   |         |     | |      | |
+// d |         | *   | |  = d | |
+//   |_________|   n | |      |_|
+//        n          |_|       1
+//                    1
+
 void matmul(float* output, float* input, float* weights, int n, int d) {
+    // weights (d,n) @ input (n,1) -> output (d,1)
     int i;
     #pragma omp parallel for private(i)
     for (i = 0; i < d; i++) {
