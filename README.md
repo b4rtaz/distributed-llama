305260e57ef6f9bf9d5bb93ec7b776dec392a297
b4rtaz <b4rtaz@gmail.com>
2024-01-08 14:47:06 +0100

    rename args.

--- README.md
+++ README.md
@@ -9,4 +9,12 @@
 3. Install dependencies of the converter `cd converter && pip install -r requirements.txt`.
 4. Convert weights to Distributed Llama format: `python converter.py /path/to/llama-2-7b q40`. This will take a bit of time.
 5. Compile Distributed Llama: `make main`.
-6. Run Distributed Llama: `./main inference -m ./converter/dllama_llama-2-7b_q40.bin -w 2 -b 3 -t ../tokenizer.bin -prompt "Hello world" -nthread 4`.
+6. Run Distributed Llama: `./main inference --model ./converter/dllama_llama-2-7b_q40.bin --tokenizer ../tokenizer.bin --weights-float-type q40 --buffer-float-type q80 --prompt "Hello world" --nthreads 4`.
+
+In the table below, you can find the expected size of the converted weights with different floating-point types.
+
+| Model       | Original size | Float32  | Float16  | Q40      |
+|-------------|---------------|----------|----------|----------|
+| Llama 2 7B  | 13,48 GB      |          |          | 3,95 GB  |
+| Llama 2 13B | 26,03 GB      |          |          | 7,35 GB  |
+| Llama 2 70B | 137,97 GB     |          |          | 36,98 GB |
--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 
 struct ProgramArgs {
     char* mode;
-    int nThread; 
+    int nThreads; 
 
     // inference
     char* modelPath;
@@ -21,6 +21,9 @@ struct ProgramArgs {
     int nWorkers;
     char** workerHosts;
     int* workerPorts;
+    float temperature;
+    float topp;
+    int steps;
 
     // worker
     int port;
@@ -37,20 +40,22 @@ int inference(ProgramArgs* args) {
         return usage();
     }
 
-    float temperature = 0.8f;
-    float topp = 0.9f;
-    int steps = 256;
-
     SocketPool socketPool = SocketPool::connect(args->nWorkers, args->workerHosts, args->workerPorts);
     unsigned int nSlices = args->nWorkers + 1;
 
     TransformerSpec spec = Transformer::loadSpecFromFile(args->modelPath, nSlices, args->weightsFloatType, args->bufferFloatType);
+
+    int steps = args->steps;
+    if (steps > spec.seqLen) {
+        steps = spec.seqLen;
+    }
+
     Transformer transformer = Transformer::loadRootFromFile(args->modelPath, &spec, &socketPool);
-    Inference inference = Inference(args->nThread, &transformer, &socketPool);
+    Inference inference = Inference(args->nThreads, &transformer, &socketPool);
 
     socketPool.enableTurbo();
 
-    generate(&spec, &inference, &socketPool, args->tokenizerPath, temperature, topp, steps, args->prompt);
+    generate(&spec, &inference, &socketPool, args->tokenizerPath, args->temperature, args->topp, steps, args->prompt);
 
     return EXIT_SUCCESS;
 }
@@ -66,10 +71,19 @@ int worker(ProgramArgs* args) {
 
     socket.enableTurbo();
 
-    Worker worker = Worker(args->nThread, &transformer, &socket);
+    Worker worker = Worker(args->nThreads, &transformer, &socket);
     worker.work();
 
     return EXIT_SUCCESS;
+}
+
+FloatType parseFloatType(char* val) {
+    if (strcmp(val, "f32") == 0) return F32;
+    if (strcmp(val, "f16") == 0) return F16;
+    if (strcmp(val, "q40") == 0) return Q40;
+    if (strcmp(val, "q80") == 0) return Q80;
+    printf("Invalid float type %s\n", val);
+    exit(EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[]) {
@@ -77,7 +91,7 @@ int main(int argc, char *argv[]) {
 
     ProgramArgs args;
     args.mode = NULL;
-    args.nThread = 4;
+    args.nThreads = 4;
     args.modelPath = NULL;
     args.tokenizerPath = NULL;
     args.prompt = NULL;
@@ -85,24 +99,25 @@ int main(int argc, char *argv[]) {
     args.bufferFloatType = F32;
     args.nWorkers = 0;
     args.port = 9990;
+    args.temperature = 0.8f;
+    args.topp = 0.9f;
+    args.steps = 64;
 
     if (argc > 1) {
         args.mode = argv[1];
     }
     for (int i = 2; i + 1 < argc; i += 2) {
-        if (strcmp(argv[i], "-nthread") == 0) {
-            args.nThread = atoi(argv[i + 1]);
-        } else if (strcmp(argv[i], "-m") == 0) {
+        if (strcmp(argv[i], "--model") == 0) {
             args.modelPath = argv[i + 1];
-        } else if (strcmp(argv[i], "-t") == 0) {
+        } else if (strcmp(argv[i], "--tokenizer") == 0) {
             args.tokenizerPath = argv[i + 1];
-        } else if (strcmp(argv[i], "-prompt") == 0) {
+        } else if (strcmp(argv[i], "--prompt") == 0) {
             args.prompt = argv[i + 1];
-        } else if (strcmp(argv[i], "-w") == 0) {
-            args.weightsFloatType = (FloatType)atoi(argv[i + 1]);
-        } else if (strcmp(argv[i], "-b") == 0) {
-            args.bufferFloatType = (FloatType)atoi(argv[i + 1]);
-        }else if (strcmp(argv[i], "-s") == 0) {
+        } else if (strcmp(argv[i], "--weights-float-type") == 0) {
+            args.weightsFloatType = parseFloatType(argv[i + 1]);
+        } else if (strcmp(argv[i], "--buffer-float-type") == 0) {
+            args.bufferFloatType = parseFloatType(argv[i + 1]);
+        } else if (strcmp(argv[i], "--workers") == 0) {
             int j = i + 1;
             for (; j < argc && argv[j][0] != '-'; j++);
             int count = j - i - 1;
@@ -126,10 +141,13 @@ int main(int argc, char *argv[]) {
             }
 
             i += count;
-        } else if (strcmp(argv[i], "-p") == 0) {
+        } else if (strcmp(argv[i], "--port") == 0) {
             args.port = atoi(argv[i + 1]);
+        } else if (strcmp(argv[i], "--nthreads") == 0) {
+            args.nThreads = atoi(argv[i + 1]);
         } else {
             printf("Unknown option %s\n", argv[i]);
+            exit(EXIT_FAILURE);
         }
     }
 
