12b9d542ae43bdba867814ed0842b947539b23d9
b4rtaz <b4rtaz@gmail.com>
2024-04-19 21:32:42 +0200

    converters.

--- converter/convert-mixtral.py
+++ converter/convert-mixtral.py
@@ -7,6 +7,7 @@ from safetensors import safe_open
 # Model: https://huggingface.co/mistral-community/Mixtral-8x22B-v0.1
 
 currentFileIndex = None
+nFiles = None
 model = None
 layerMap = {}
 folderPath = None
@@ -20,13 +21,14 @@ def unloadModel():
 
 def loadModel(index):
     global currentFileIndex
+    global nFiles
     global model
     global layerMap
     global folderPath
     if (currentFileIndex == index):
         return
     unloadModel()
-    fileName = f'model-000{str(index).zfill(2)}-of-00059.safetensors' + '?download=true'
+    fileName = f'model-000{str(index).zfill(2)}-of-000{nFiles}.safetensors'# + '?download=true'
     filePath = os.path.join(folderPath, fileName)
     print(f'💿 Loading file {fileName}...')
     model = safe_open(filePath, framework='pt', device='cpu')
@@ -53,26 +55,43 @@ def writeLayer(outFile, layerName, targetFloatType):
     print(f'🔶 Writing tensor {layerName} {tensor.shape}...')
     writeTensor(outFile, tensor, targetFloatType)
 
-def convert(targetFloatType, outputFileName):
-    outFile = open(outputFileName, 'wb')
-
+def getParams(modelName):
     params = {
         'arch_type': 0xABCD02,
-        'dim': 6144,
-        'hidden_dim': 16384,
-        'n_layers': 56,
-        'n_heads': 48,
-        'n_kv_heads': 8,
+        'vocab_size': 32000,
         'n_experts': 8,
         'n_active_experts': 2,
-        'vocab_size': 32000,
-        'max_seq_len': 65536,
         'hidden_act': 1, # silu
-        'rope_theta': 1000000
+        'rope_theta': 1000000,
     }
+    if (modelName == '8x7B'):
+        params['dim'] = 4096
+        params['hidden_dim'] = 14336
+        params['n_layers'] = 32
+        params['n_heads'] = 32
+        params['n_kv_heads'] = 8
+        params['max_seq_len'] = 32768
+        params['n_files'] = 19
+    elif (modelName == '8x22B'):
+        params['dim'] = 6144
+        params['hidden_dim'] = 16384
+        params['n_layers'] = 56
+        params['n_heads'] = 48
+        params['n_kv_heads'] = 8
+        params['max_seq_len'] = 65536
+        params['n_files'] = 59
+    else:
+        raise Exception(f'Unknown model {modelName}')
+    return params
+
+def convert(modelName, targetFloatType, outputFileName):
+    global nFiles
+    params = getParams(modelName)
+    nFiles = params['n_files']
+
+    outFile = open(outputFileName, 'wb')
     writeHeader(outFile, params)
 
-    #### model-00001-of-00059.safetensors -> model-00059-of-00059.safetensors
     loadModel(1)
 
     writeLayer(outFile, 'model.embed_tokens.weight', 'f32')
@@ -92,7 +111,7 @@ def convert(targetFloatType, outputFileName):
         writeLayer(outFile, f'model.layers.{index}.input_layernorm.weight', 'f32')
         writeLayer(outFile, f'model.layers.{index}.post_attention_layernorm.weight', 'f32')
 
-    loadModel(59)
+    loadModel(nFiles)
 
     writeLayer(outFile, 'model.norm.weight', 'f32')
     writeLayer(outFile, 'lm_head.weight', targetFloatType)
@@ -103,19 +122,20 @@ def convert(targetFloatType, outputFileName):
     print(f'Converted {outputFileName}')
 
 def usage():
-    print('Usage: python convert-mixtral.py <modelPath> <targetFloatType>')
+    print('Usage: python convert-mixtral.py <modelName> <modelPath> <targetFloatType>')
     exit(1)
 
 if __name__ == '__main__':
-    if (len(sys.argv) < 3):
+    if (len(sys.argv) < 4):
         usage()
 
-    folderPath = sys.argv[1]
-    targetFloatType = sys.argv[2]
-    outputFileName = f'dllama-mixtral-{targetFloatType}.bin'
+    modelName = sys.argv[1]
+    folderPath = sys.argv[2]
+    targetFloatType = sys.argv[3]
+    outputFileName = f'dllama_mixtral_{modelName.lower()}-{targetFloatType}.bin'
 
     if not isFloatTypeSupported(targetFloatType):
         print('Float type is not supported')
         exit(1)
 
-    convert(targetFloatType, outputFileName)
+    convert(modelName, targetFloatType, outputFileName)
---
+++ converter/convert-tokenizer-sentencepiece.py
@@ -0,0 +1,70 @@
+import os
+import struct
+import sys
+from typing import List
+from sentencepiece import SentencePieceProcessor
+
+class Tokenizer:
+    def __init__(self, model_path):
+        assert os.path.isfile(model_path), model_path
+        self.sp_model = SentencePieceProcessor(model_file=model_path)
+        self.model_path = model_path
+
+        # BOS / EOS token IDs
+        self.n_words: int = self.sp_model.vocab_size()
+        self.bos_id: int = self.sp_model.bos_id()
+        self.eos_id: int = self.sp_model.eos_id()
+        self.pad_id: int = self.sp_model.pad_id()
+        print(f"#words: {self.n_words} - BOS ID: {self.bos_id} - EOS ID: {self.eos_id} - PAD ID: {self.pad_id}")
+        assert self.sp_model.vocab_size() == self.sp_model.get_piece_size()
+
+    def encode(self, s: str, bos: bool, eos: bool) -> List[int]:
+        assert type(s) is str
+        t = self.sp_model.encode(s)
+        if bos:
+            t = [self.bos_id] + t
+        if eos:
+            t = t + [self.eos_id]
+        return t
+
+    def decode(self, t: List[int]) -> str:
+        return self.sp_model.decode(t)
+
+    def export(self):
+        # get all the tokens (postprocessed) and their scores as floats
+        tokens, scores = [], []
+        for i in range(self.n_words):
+
+            # decode the token and light postprocessing
+            t = self.sp_model.id_to_piece(i)
+            s = self.sp_model.get_score(i)
+            if i == self.bos_id:
+                t = '\n<s>\n'
+            elif i == self.eos_id:
+                t = '\n</s>\n'
+            t = t.replace('▁', ' ') # sentencepiece uses this character as whitespace
+            b = t.encode('utf-8') # bytes of this token, utf-8 encoded
+
+            tokens.append(b)
+            scores.append(s)
+
+        # record the max token length
+        max_token_length = max(len(t) for t in tokens)
+ 
+        # write to a binary file
+        # the tokenizer.bin file is the same as .model file, but .bin
+        outputPath = os.path.basename(self.model_path).replace('.model', '.t')
+        with open(outputPath, 'wb') as f:
+            f.write(struct.pack("I", max_token_length))
+            for bytes, score in zip(tokens, scores):
+                print(f"{bytes.decode('utf-8')} {score}")
+                f.write(struct.pack("fI", score, len(bytes)))
+                f.write(bytes)
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print('Invalid usage')
+        exit(1)
+
+    t = Tokenizer(sys.argv[1])
+    t.export()
